ee585a8abba3a696eddb66ebd7e2cf1b758a6009
Ridham2808 <ridham.patel.cg@gmail.com>
2025-01-10 11:23:03 +0530

    add link

--- Crud_Operations/students.js
+++ Crud_Operations/students.js
@@ -5,7 +5,8 @@ const app = express();
 const port = 3000;
 
 // MongoDB connection details
-const uri = "mongodb://127.0.0.1:27017"; 
+const uri_local = "mongodb://127.0.0.1:27017"; 
+const uri = "mongodb+srv://test:test123@cluster0.i9vhn.mongodb.net/"
 const dbName = "codinggita";
 
 // Middleware
